fdb274a7947ca86bd0e77d7c51a98658e6baa31c
KeoFoxy <arasdiel68@gmail.com>
2025-09-02 13:25:36 +0300

    upd vim diagnostic

--- lua/options.lua
+++ lua/options.lua
@@ -1,17 +1,18 @@
 require "nvchad.options"
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 vim.diagnostic.config({
   virtual_text = {
     prefix = "●",
     spacing = 4,
   },
-  signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+  },
   underline = true,
   update_in_insert = false,
   severity_sort = true,
@@ -21,6 +22,5 @@ vim.o.updatetime = 250
 vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 
 -- add yours here!
-
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
